67e3fe8965b1ea4af5c4524ae4cb14a19ff38e87
Dominik Wille <dominik.wille@fu-berlin.de>
2013-10-25 10:36:52 +0200

    refactored some code....

--- sheet0/rechteck.py
+++ sheet0/rechteck.py
@@ -3,36 +3,35 @@
 
 def area (a, b):
     if ( a >= 0 and b >= 0 ):
-        return a * b;
+        return a * b
     else:
         return -1;
 
 def isfloat (x):
     try:
         float(x);
-        return True;
+        return True
     except:
-        return False;
+        return False
 
 def print_area (a, b):
-    c = area(a, b);
+    c = area(a, b)
     if(c >= 0):
-        return str(c);
-    return 'Fehler bei der Berechnung - überprüfen sie ihre Eingaben';
+        return str(c)
+    return 'Fehler bei der Berechnung - überprüfen sie ihre Eingaben'
 
 def input_n (str):
-    x = 'foo';
+    x = 'foo'
     first = True;
     while(not isfloat(x) or x < 0):
         if(first):
-            first = False;
+            first = False
         else:
-            print 'Das ist keine Zahl!';
+            print 'Das ist keine Zahl!'
         x = raw_input(str);
-    return float(x);
+    return float(x)
 
-
-a = input_n('Länge a des Rechtecks: ');
-b = input_n('Länge b des Rechtecks: ');
+a = input_n('Länge a des Rechtecks: ')
+b = input_n('Länge b des Rechtecks: ')
 
 print "das rechteck hat einen Flächeninhalt von: %s" % (print_area(a, b))
