4aa89b15066d35226727e7e27fe39033ba804428
Cukhen <herrcukhen@gmail.com>
2013-11-08 16:32:57 +0100

    Formulierung als Funktionen

--- sheet3/gauss.py
+++ sheet3/gauss.py
@@ -7,6 +7,9 @@ import numpy as np
 #LGS einlesen
 F = np.matrix('1.,-2.,-1.;36000.,2.,0.;-2.,1400.,1.')
 d = np.matrix('3.;72002.;1399.')
+#Kontrollausgaben:
+#print F
+#print d
 
 #Vernichtungsfunktion
 def eli(n,m):
@@ -15,16 +18,19 @@ def eli(n,m):
     d[n] = d[n]-mod*d[m]
 
 
-#Vernichtungsfunktion schleift über LGS
-for j in range (0,F.shape[0]-1):
-    for i in range (j+1,F.shape[0]):
-        eli(i,j)
+#Stufenmatrix, liefert in erster Komponente die Stufenform und in der zweiten den
+#gewandelten Ergebnissvektor
+def stufenmatrix(F,d):
+    for j in range (0,F.shape[0]-1):
+        for i in range (j+1,F.shape[0]):
+            eli(i,j)
+    return F,d
         
-#Ausgabe Stufenform
-print F
-print d
+#Kontrollausgaben
+#print F
+#print d
 
-#Kontrollausgabe
+#Kontrollausgaben
 #c=d[2]/F[2,2]
 #print c
 #b=(d[1]-F[1,2]*c)/F[1,1]
@@ -33,11 +39,14 @@ print d
 #print c
 
 #Berechnung des Lösungsvektors
-l = np.matrix ('0.;0.;0.')
-mod = 0
-for i in range(2, -1, -1):
-    for j in range(i,2):
-        mod = mod+F[i,j+1]*l[j+1]
-    l[i] = (d[i]-mod)/F[i,i]
+def loesungsvektor(F,d):
+    l = np.matrix ('0.;0.;0.')
     mod = 0
-print l[::-1]
+    for i in range(F.shape[0]-1, -1, -1):
+        for j in range(i,F.shape[0]-1):
+            mod = mod+F[i,j+1]*l[j+1]
+        l[i] = (d[i]-mod)/F[i,i]
+        mod = 0
+    return l[::-1]
+
+print loesungsvektor(stufenmatrix(F,d)[0],stufenmatrix(F,d)[1])    
