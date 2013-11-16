f7670791d0b06bac2636318429ce4871e114f69e
Dominik Wille <dominik.wille@fu-berlin.de>
2013-11-16 16:22:12 +0100

    A2.1 done init A2.2

--- sheet4/a2.py
+++ sheet4/a2.py
@@ -6,8 +6,21 @@
 # @tutor Alexander Schlaich
 # @sheet 4
 
-#4.2
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pyplot as plt
 
-def psi(x, y):
-    x**4 - x**2 + y**4 - 0.2 * y**3 - y**2 + 0.2 x * y**3
+#4.2
+def phi(x, y):
+    return x**4 - x**2 + y**4 - 0.2 * y**3 - y**2 + 0.2 * x * y**3
+
+def F(x, y):
+    return np.matrix([[-4.0*x**3 + 2.0*x - 0.2*y**3]], [[-4.0*y**3 + 0.6*y**2 + 2.0*y - 0.6*x*y**2]])
+    
+x = np.arange(-1.0, 1.01, 0.1)
+y = np.arange(-1.0, 1.01, 0.1)
+
+x, y = np.meshgrid(x, y)
+Axes3D(plt.figure()).plot_wireframe(x, y, np.vectorize(phi)(x, y))
+plt.show()
 
