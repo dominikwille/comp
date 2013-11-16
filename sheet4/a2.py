591017072f226acb5a328f0fd8edccd43cd0dee0
Dominik Wille <dominik.wille@fu-berlin.de>
2013-11-16 15:39:42 +0100

    init sheet 4

---
+++ sheet4/a2.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+#
+# @author Dominik Wille
+# @author Stefan Pojtinger
+# @tutor Alexander Schlaich
+# @sheet 4
+
+#4.2
+
+def psi(x, y):
+    x**4 - x**2 + y**4 - 0.2 * y**3 - y**2 + 0.2 x * y**3
+
