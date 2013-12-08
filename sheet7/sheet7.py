432818023c9289dba2a54c1bf3e8230534178b42
Cukhen <herrcukhen@gmail.com>
2013-12-08 12:06:16 +0100

    7.1.3

--- sheet7/sheet7.py
+++ sheet7/sheet7.py
@@ -7,9 +7,19 @@
 # @sheet 6
 #
 #Packete einlesen:
-
+import numpy as np
 
 #7.1.2
 def D(func,x,h):
+	return (func(x+h)-func(x))/h
+
+
+def D2(func,x,h):
 	return (func(x+h)-func(x-h))/(2*h)
 
+#7.1.3
+def f(x):
+	return 1/(2+np.cos(x))
+	
+x = np.array[0,2*np.pi]
+print x
